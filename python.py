c2810e81c757c4c25323b054a797b5dabc1425d2
Szymon Radomski <107359025+Pythadela@users.noreply.github.com>
2022-10-24 00:04:42 +0200

    Added snake motion and apples.

--- python.py
+++ python.py
@@ -1,30 +1,116 @@
 import pygame
+import sys
+import random
 
-size = 500
-rows = 20
+
+class Cube:
+    rows = 20
+
+    def __init__(self, start, dirx=1, diry=0, color=(0, 255, 0)):
+        self.pos = start
+        self.dirx = dirx
+        self.diry = diry
+        self.color = color
+
+    def move(self, dirx, diry):
+        self.dirx = dirx
+        self.diry = diry
+        self.pos = (self.pos[0] + self.dirx, self.pos[1] + self.diry)
+
+    def draw(self, surface):
+        dis = size // rows
+        rw = self.pos[0]
+        cm = self.pos[1]
+        pygame.draw.rect(surface, self.color, (rw * dis + 1, cm * dis + 1, dis, dis))
 
 
-class Snake():
+class Snake:
     body = []
+    turns = {}
 
     def __init__(self, color, pos):
         self.color = color
-        self.head = pos
+        self.head = Cube(pos)
         self.body.append(self.head)
         self.dirx = 0
         self.diry = 1
 
     def move(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-    def reset(self, pos):
-        pass
+            keys = pygame.key.get_pressed()
 
+            if keys[pygame.K_LEFT]:
+                self.dirx = -1
+                self.diry = 0
+                self.turns[self.head.pos] = [self.dirx, self.diry]
+
+            elif keys[pygame.K_RIGHT]:
+                self.dirx = 1
+                self.diry = 0
+                self.turns[self.head.pos] = [self.dirx, self.diry]
+
+            elif keys[pygame.K_UP]:
+                self.dirx = 0
+                self.diry = -1
+                self.turns[self.head.pos] = [self.dirx, self.diry]
+
+            elif keys[pygame.K_DOWN]:
+                self.dirx = 0
+                self.diry = 1
+                self.turns[self.head.pos] = [self.dirx, self.diry]
+
+        for i, c in enumerate(self.body):
+            p = c.pos
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+                if i == len(self.body) - 1:
+                    self.turns.pop(p)
+            else:
+                if c.dirx == -1 and c.pos[0] <= 0:
+                    c.pos = (c.rows - 1, c.pos[1])
+
+                elif c.dirx == 1 and c.pos[0] >= c.rows - 1:
+                    c.pos = (0, c.pos[1])
+
+                elif c.diry == 1 and c.pos[1] >= c.rows - 1:
+                    c.pos = (c.pos[0], 0)
+
+                elif c.diry == -1 and c.pos[1] <= 0:
+                    c.pos = (c.pos[0], c.rows - 1)
+
+                else:
+                    c.move(c.dirx, c.diry)
+        if len(Snake.body) > 2:
+            self.reset()
+
+            
     def add_cube(self):
-        pass
+        tail = self.body[-1]
+        dx, dy = tail.dirx, tail.diry
+
+        if dx == 1 and dy == 0:
+            self.body.append(Cube((tail.pos[0] - 1, tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(Cube((tail.pos[0] + 1, tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(Cube((tail.pos[0], tail.pos[1] - 1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(Cube((tail.pos[0], tail.pos[1] + 1)))
+
+        self.body[-1].dirx = dx
+        self.body[-1].diry = dy
 
     def draw(self, surface):
-        pass
+        for i, c in enumerate(self.body):
+            if i == 0:
+                c.draw(surface)
+            else:
+                c.draw(surface)
 
 
 def draw_grid(width, rows, surface):
@@ -41,24 +127,39 @@ def draw_grid(width, rows, surface):
 
 def draw_window(surface):
     surface.fill((0, 0, 0))
+    s.draw(surface)
+    apple.draw(surface)
     draw_grid(size, rows, surface)
     pygame.display.update()
 
 
+def random_apple(snake):
+    positions = snake.body
+    while True:
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+        if len(list(filter(lambda z: z.pos == (x, y), positions))) > 0:  #
+            continue
+        else:
+            break
+    return x, y
+
+
 def main():
-    window = pygame.display.set_mode((size, size))
+    global size, rows, s, apple
+    size = 500
+    rows = 20
+    window = pygame.display.set_mode((501, 501))
 
-    s = Snake((0, 0, 0), (10, 10))
+    s = Snake((0, 255, 0), (10, 10))
+    apple = Cube(random_apple(s), color=(255, 0, 0))
 
-    run = True
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.display.update()
-
-        pygame.time.delay(50)
-        pygame.time.Clock().tick(60)
+    while True:
+        s.move()
+        pygame.time.delay(100)
+        if s.body[0].pos == apple.pos:
+            s.add_cube()
+            apple = Cube(random_apple(s), color=(255, 0, 0))
 
         draw_window(window)
 
